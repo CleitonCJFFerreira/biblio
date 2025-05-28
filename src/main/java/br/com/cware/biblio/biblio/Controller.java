4c6bc70cea0770cbc3bbecb2618d065b4f7e39df
cleiton <cleitonjferreira@hotmail.com>
2025-05-27 23:09:36 -0300

    Criando projeto

---
+++ src/main/java/br/com/cware/biblio/biblio/Controller.java
@@ -0,0 +1,16 @@
+package br.com.cware.biblio.biblio;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas(){
+        return "Essa é a primeira rota";
+    }
+
+}
---
+++ src/test/java/br/com/cware/biblio/biblio/BiblioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cware.biblio.biblio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
